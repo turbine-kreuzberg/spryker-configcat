1209dff34de48d8206ff647d34b0089df77ff2f5
benkuhn <ben.kuhn@turbinekreuzberg.com>
2025-04-30 09:09:53 +0200

    - fix

--- tests/Unit/TurbineKreuzberg/Client/FeatureFlag/Cache/ConfigCatCacheTest.php
+++ tests/Unit/TurbineKreuzberg/Client/FeatureFlag/Cache/ConfigCatCacheTest.php
@@ -47,8 +47,7 @@ class ConfigCatCacheTest extends TestCase
         self::assertFalse(
             $configCatCache->set(
                 'testKey',
-                '
-                testValue',
+                'testValue',
                 (new DateInterval('P7D')),
             ),
         );
